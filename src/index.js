9204efd810e7318c51ac1e87937184deeff545ad
Raymi Saldomando <raymi15@gmail.com>
2018-11-28 19:14:07 -0500

    Save the App State to Local Storage

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
-import 'babel-polyfill';
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import throttle from 'lodash/throttle';
 import todoApp from './reducers';
 import App from './components/App';
+import { loadState, saveState } from './lib/localStorage';
 
-const store = createStore(todoApp);
+const persistedState = loadState();
+const store = createStore(todoApp, persistedState);
+
+/*
+Use throtle lodash function utility to ensure that the saveState function gets call time every second
+*/
+
+store.subscribe(
+  throttle(() => {
+    saveState({
+      todos: store.getState().todos,
+    });
+  }, 1000)
+);
 
 render(
   <Provider store={store}>
